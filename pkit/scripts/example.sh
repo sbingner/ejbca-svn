c2d6893adf89c09cf9dbb4ef13174b78d261776a
kimbogum <kimbogum@37aeb1de-b14b-0410-9a0e-82b28170d228>
2003-09-01 03:55:09 +0000

    made relevant to new changes
    git-svn-id: https://svn.cesecore.eu/svn/ejbca/trunk@1085 37aeb1de-b14b-0410-9a0e-82b28170d228

--- pkit/scripts/example.sh
+++ pkit/scripts/example.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# an example shell script to run CertificateTool from the commandline on Linux/Unix
 CP=${CLASSPATH}:../lib/bcprov-jdk14-115.jar:../lib/ca.jar:../lib/log4j-1.2.jar:./dist/pkit.jar
-exec java -cp ${CP} pkit.examples.CertificateTool "../etc/signer.properties" 
+exec java -cp ${CP} pkit.examples.CertificateTool
